609c3d8f423b34be3227e51c091caeb90daa682f
9124104188-art <9124104188@kcgcollege.com>
2026-06-02 20:13:24 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -1,1 +1,23 @@
 //server goes here
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post('api/review', (req, res) => {
+    const { language, code } = req.body;
+
+    if(!code){
+        return res.status(400).json({ error: 'Code is required.' });
+    }
+
+    res.status(200).json({ review: 'Fake review for ${language}: Your code looks good!' });
+});
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log('Server is running on port ${PORT}');
+});
