d102f4ff05d4ff1da9e2c1dc260fe07c17e2124e
9124104188-art <9124104188@kcgcollege.com>
2026-06-02 19:58:20 +0530

    updated

--- client/app.js
+++ client/app.js
@@ -16,6 +16,25 @@ form.addEventListener('submit', async (e) => {
     else{
         output.textContent = 'Reviewing code...';
         console.log({language, codeLength: code.length});
+        try{
+            fetch('http://localhost:3000/api/review', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ language, code })
+        })
+
+        const data = await response.json();
+        output.textContent = data.review||'No review generated.';
+
+        }
+        catch(error){
+            output.textContent = 'Error reviewing code. Please try again.';
+            console.error('Error:', error);
+        }
+        
+        
     }
 
 });
