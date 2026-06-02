8a7b339f1f488755a7f769f16a35f0031c4d72f3
9124104188-art <9124104188@kcgcollege.com>
2026-06-02 20:27:35 +0530

    updated

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, '../client')));
 app.post('/api/review', (req, res) => {
     const { language, code } = req.body;
 
